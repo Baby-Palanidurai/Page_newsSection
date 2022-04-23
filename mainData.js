3f44cb8ba91b80b91d746f729689c7101b20e61b
Baby-Palanidurai <Baby-Palanidurai@users.noreply.github.com>
2022-04-23 22:57:54 +0530

    Create mainData.js

---
+++ mainData.js
@@ -0,0 +1,7 @@
+{
+  "data": [
+    { "sectionId": "fashion", "sectionName": "FASHION", "sectionUrl": "" },
+    { "sectionId": "travel", "sectionName": "TRAVAL", "sectionUrl": "" },
+    { "sectionId": "beauty", "sectionName": "BEAUTY", "sectionUrl": "" }
+  ]
+}
